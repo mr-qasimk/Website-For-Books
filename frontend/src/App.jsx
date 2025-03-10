723ad5d6ed8668ac9ec47c336b322e070d120cd1
khanqasim123 <qasim2905@gmail.com>
2025-03-10 14:05:00 +0500

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Dashboard from './Pages/Dashboard'
 import YourFavorite from './Pages/YourFavorite'
-import SignInModal from './Components/SignInModal';
-import SignUpFrom from './Components/SignUpForm'
 import PageNotFound from './Pages/PageNotFound';
 
 
@@ -17,14 +15,7 @@ const router = createBrowserRouter([
     path: "/yourfavorite",
     element: <YourFavorite />,
   },
-  {
-    path: "/signin",
-    element: <SignInModal />,
-  },
-  {
-    path: "/signup",
-    element: <SignUpFrom />,
-  },
+  
   {
     path: "*", // Catch-all for undefined routes
     element: <PageNotFound />,
